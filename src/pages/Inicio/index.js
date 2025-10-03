028dcbe470f84a4865fa2863fdea11da0896e76c
neosamdez <leonardoaguiar080@gmail.com>
2025-10-02 22:43:05 -0300

    card dinamico com json integrado

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,10 @@
 import Banner from 'components/Banner'
 import Cabecalho from 'components/Cabecalho';
+import Card from 'components/Card';
 import Footer from 'components/Footer';
 import Titulo from 'components/Titulo';
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
 function Inicio() {
     return(
@@ -11,6 +14,11 @@ function Inicio() {
             <Titulo>
                 <h1>um lugar para guardar seus videos e filmes</h1>
             </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Footer />
         </>    
     );
